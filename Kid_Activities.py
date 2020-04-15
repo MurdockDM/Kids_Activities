037c5526cd9ed0aa0724e1a15349d5a18dbdb4b2
Dustin Murdock <dmurdock2@gmail.com>
2020-04-15 00:00:57 -0500

    initial commit

---
+++ Kid_Activities.py
@@ -0,0 +1,25 @@
+def run(*kids):
+    for name in kids:
+        print(f"{name} ran like a fool!")
+
+run("Pam", "Sam", "Andrea", "Will")
+
+
+def swing(*kids):
+    for name in kids:
+        print(f"{name} was swinging like a fool!")
+
+swing("Marybeth", "Ariel", "Kevin", "Courtney")
+
+
+def slide(*kids):
+    for name in kids:
+        print(f"{name} was sliding like a fool!")
+
+slide("Mike", "Jack", "Jennifer", "Earl")
+
+def hide(*kids):
+    for name in kids:
+        print(f"{name} was hiding like a fool!")
+
+hide("Henry", "Heather", "Hayley", "Hugh")
